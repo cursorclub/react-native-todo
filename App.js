1c2c95b5a9cf555ef569704ceed26325e063e0e4
Nicholas Carr <ncarr@users.noreply.github.com>
2018-07-04 23:08:26 -0400

    Added an icon button and required styles

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { Constants } from 'expo';
+import { Icon } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          placeholder="What do you want to do?"
-        />
+        <View style={styles.textBox}>
+          <TextInput
+            placeholder="What do you want to do?"
+            style={styles.textInput}
+          />
+          <Icon
+            name="add"
+            iconStyle={styles.icon}
+          />
+        </View>
       </View>
     );
   }
@@ -19,5 +27,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     marginTop: Constants.statusBarHeight,
+  },
+  textBox: {
+    flexDirection: 'row',
+  },
+  textInput: {
+    flexGrow: 1,
+  },
+  icon: {
+    padding: 10,
   },
 });
