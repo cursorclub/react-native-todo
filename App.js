2d3c626393ff2fc4fbfad2ad6dc9ffb53ec05bf1
Nicholas Carr <ncarr@users.noreply.github.com>
2018-07-04 23:19:52 -0400

    Initialized state

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { Constants } from 'expo';
 import { Icon } from 'react-native-elements';
+import uuid from 'uuid';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { key: uuid.v4(), done: true, text: 'Host this workshop' },
+        { key: uuid.v4(), done: false, text: 'Do something else' },
+      ],
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
