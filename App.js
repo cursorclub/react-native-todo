0ed50db4d68a47ccf6b3cc9e25d85178562f285f
Nicholas Carr <ncarr@users.noreply.github.com>
2018-07-04 22:45:02 -0400

    Removed centre alignment styles

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
